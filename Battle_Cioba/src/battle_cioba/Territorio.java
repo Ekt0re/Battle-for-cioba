cc943542bc00726fe668821e843d513f030c0ee1
Simone-Marangoni <smarangoni805@gmail.com>
2025-03-07 15:58:50 +0100

    Update Project

--- Battle_Cioba/src/battle_cioba/Territorio.java
+++ Battle_Cioba/src/battle_cioba/Territorio.java
@@ -4,10 +4,25 @@
  */
 package battle_cioba;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author hp
  */
-public class Territorio {
+public abstract class Territorio {
+    //private Random rnd = new Random();
+    protected String name;
+    protected static String id = "-1";
+    
+    private String statoPadrone;  //Nome Stato che possiede questo territorio
+    protected boolean acqua = true;
+    //protected boolean terra;  all'inizio tutto acqua
+    protected List<String> risorse; //Nel costruttore quando territorio viene creato
+    protected long numCivili;   //Long xchè int MAX è 2Miliardi
+    protected String regione;
+    protected int countBasiMilitari = 0;    //Numero basi militari
+    
     
 }
